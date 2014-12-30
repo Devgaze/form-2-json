ae247637ec46800f3798bbc00fd285c873eea6e4
Alen Novakovic <alen@devgaze.com>
2014-12-29 22:49:13 -0800

    now it reads dot notation - still doesn't cover everything

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 form-2-json
 ===========
 
-Reads the form and creates a beautiful JSON object.
+Lightweight & fast, pure Javascript, plugin for serializing HTML forms as a key/value pair (JSON) object.
+The main idea behind this plugin is the ability to understand `dot notation` inside of the `name` attribute, for more meaningful output.
+
+For the sake of simplicity I'll keep this module as simple and light as possible. If you have some ideas, we can discuss about integrating it inside, or you can just fork it and evolve it yourself.
--- src/index.js
+++ src/index.js
@@ -1,24 +1,105 @@
 ;(function (ns) {
+
   'use strict';
 
-  ns.el       = null,
-  ns.o        = {},
+  var obj     = {};
   ns.settings = {
-    delimiter: "."
-  };
+                  delimiter: "."
+                };
 
   ns.load = function (el){
+    ns.o = {};
+
     if(ns.el){
-      ns.o = {}; // Firefox needs this
+      // ns.o = {}; // Firefox needs this
     } else {
       ns.el = document.getElementById(el);
+      ns.items  = ns.el.elements;
     }
-    ns.items  = ns.el.elements;
-    parseForm();
-    console.log(JSON.stringify(ns.o))
+
+    mapForm();
+    ns.o = JSON.stringify(ns.o, undefined, 2);
   }
 
-  // privates
+  function mapForm(){
+    for (var i=0; i<=ns.items.length; i++){
+      var currEl = ns.items[i];
+      if (typeof currEl !== 'undefined' && currEl.type != 'fieldset') {
+        var propName  = currEl.getAttribute('name'),
+            propValue = getElValue(currEl);
+
+        if (propName.indexOf(ns.settings.delimiter) !== -1){
+          delimitedToObject(obj, propName.split(ns.settings.delimiter), propValue);
+          objectToOutput(obj, ns.o);
+        } else {
+          ns.o[propName] = propValue;
+        }
+      }
+    }
+  }
+
+  function delimitedToObject (obj, path, value){
+    var p = path, l = p.length - 1;
+    for (var i = 0; i < l; i++) {
+      obj = obj[p[i]] = {};
+    }
+    obj[p[l]] = value;
+  }
+
+  function objectToOutput(o, output){
+    for (var p in o){
+      if(p in output){
+        if(typeof o[p] == 'object'){
+          objectToOutput(o[p], output[p]);
+        } else {
+          output[p] = o[p]
+        }
+      } else {
+        output[p] = o[p];
+      }
+    }
+  }
+
+  function getElValue(currEl){
+    var v   = null,
+        lv  = [];
+
+    if (typeof currEl !== 'undefined') {
+      switch(currEl.type){
+        case 'text':
+        case 'radio':
+        case 'textarea':
+        case 'select-one':
+          v = currEl.value || '';
+          break;
+        case 'checkbox':
+          console.log(currEl.value)
+          if(currEl.value != '' && currEl.value != 'on'){
+            if (currEl.checked) {
+              v = currEl.value;
+            } else {
+              v = currEl.checked;
+            }
+          } else {
+            v = currEl.checked;
+          }
+          // v = (currEl.value != "" && currEl.value != "on") ? (currEl.checked) ? currEl.value : currEl.checked : currEl.checked ;
+          break;
+        case 'select-multiple':
+          for(var i=0; i<=currEl.options.length-1; i++){
+            if (typeof currEl.options[i] != 'undefined' && currEl.options[i].selected){
+              lv.push(currEl.options[i].value);
+            }
+          }
+          v = lv;
+          break;
+        default:
+          break;
+      }
+    }
+    return v;
+  }
+
   function extend(x, y) {
     for(var k in y) {
       if(y.hasOwnProperty(k)) {
@@ -26,52 +107,6 @@
       }
     }
     return x;
-  }
-
-  function parseForm(){
-    for (var i=0; i<=ns.items.length; i++){
-
-      var currEl = ns.items[i];
-
-      if (typeof currEl !== 'undefined') {
-
-        // handle different value attributes
-        var listValues    = [],
-            propName      = currEl.getAttribute('name'),
-            checkSiblings = document.getElementsByName(propName).length;
-
-        switch(currEl.type){
-          case 'text':
-          case 'radio':
-          case 'textarea':
-          case 'select-one':
-            ns.o[propName] = currEl.value;
-            break;
-          case 'checkbox':
-            if (!ns.o[propName]){
-              ns.o[propName] = (checkSiblings > 1) ? [] : currEl.checked;
-            }
-            console.log (checkSiblings)
-            if(checkSiblings > 1){
-              if (currEl.checked){
-                // if (currEl.value != "" && currEl.value != "on")
-                ns.o[propName].push(currEl.value);
-              }
-            }
-            break;
-          case 'select-multiple':
-            for(var j=0; j<=currEl.options.length-1; j++){
-              if (typeof currEl.options[j] != 'undefined' && currEl.options[j].selected){
-                listValues.push(currEl.options[j].value);
-              }
-            }
-            ns.o[currEl.getAttribute('name')] = listValues;
-            break;
-          default:
-            break;
-        }
-      }
-    }
   }
 
 })( window.Form2JSON = window.Form2JSON || {} );
