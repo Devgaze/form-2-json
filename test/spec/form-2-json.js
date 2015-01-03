772ba56a0d8805b53f1a3383e762c217daf9d1e5
Alen Novakovic <alen@devgaze.com>
2015-01-02 22:01:26 -0800

    barebone test setup

---
+++ test/spec/form-2-json.js
@@ -0,0 +1,12 @@
+describe("Testing Form-2-JSON", function(){
+
+	beforeEach(function(){
+	});
+
+	afterEach(function(){
+	});
+
+	it('module is loaded', function(){
+		expect(typeof Form2JSON).toBe("object")
+	});
+});
